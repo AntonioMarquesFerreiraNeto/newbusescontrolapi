d234828558cca5a044aa0e35170ac3561f103199
Antonio Marques <antoniomarquesfneto@gmail.com>
2024-06-27 20:39:26 -0300

    incident: test

--- BusesControl.Business/obj/Debug/net8.0/BusesControl.Business.AssemblyInfo.cs
+++ BusesControl.Business/obj/Debug/net8.0/BusesControl.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusesControl.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296e1c4e0c7b77454aff8300f768e76144b76fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a16a7e11864b451bab678555bca046cecc79eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusesControl.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusesControl.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BusesControl.Entities/obj/Debug/net8.0/BusesControl.Entities.AssemblyInfo.cs
+++ BusesControl.Entities/obj/Debug/net8.0/BusesControl.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusesControl.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fa69e21879ee782e50c71cf7fe4d073c4c9998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a16a7e11864b451bab678555bca046cecc79eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusesControl.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusesControl.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
