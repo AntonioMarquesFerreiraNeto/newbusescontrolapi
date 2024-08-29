7b16f3156964c9775af204b2af082ca7b3fd0124
Antonio Marques <antoniomarquesfneto@gmail.com>
2024-08-29 19:04:33 -0300

    refactor: ajusta service do cache usando redis

--- BusesControl.Services/v1/CacheService.cs
+++ BusesControl.Services/v1/CacheService.cs
@@ -11,26 +11,44 @@ public class CacheService(
 {
     public async Task<T?> GetAsync<T>(string key)
     {
-        var cachedData = await _distributedCache.GetAsync(key);
-        if (cachedData is null)
+        try
+        {
+            var cachedData = await _distributedCache.GetAsync(key);
+            if (cachedData is null)
+            {
+                return default!;
+            }
+
+            var serializedData = Encoding.UTF8.GetString(cachedData);
+            return JsonSerializer.Deserialize<T>(serializedData);
+        }
+        catch (Exception)
         {
             return default!;
         }
-
-        var serializedData = Encoding.UTF8.GetString(cachedData);
-        return JsonSerializer.Deserialize<T>(serializedData);
     }
 
     public async Task SetAsync<T>(string key, T value, DistributedCacheEntryOptions options)
     {
-        var serializedData = JsonSerializer.Serialize(value);
-        var encodedData = Encoding.UTF8.GetBytes(serializedData);
-        await _distributedCache.SetAsync(key, encodedData, options);
+        try 
+        {
+            var serializedData = JsonSerializer.Serialize(value);
+            var encodedData = Encoding.UTF8.GetBytes(serializedData);
+            await _distributedCache.SetAsync(key, encodedData, options);
+        }
+        catch (Exception) { }
     }
 
     public async Task<bool> RemoveAsync(string key)
     {
-        await _distributedCache.RemoveAsync(key);
-        return true;
+        try 
+        {
+            await _distributedCache.RemoveAsync(key);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
