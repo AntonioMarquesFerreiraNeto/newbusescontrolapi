7768a2aa7f12540bf622a9a72b351395116c3a41
Antonio Marques <antoniomarquesfneto@gmail.com>
2025-03-15 00:51:51 -0300

    refactor: remove if do generate base 64

--- BusesControl.Commons/GenerateBase64.cs
+++ BusesControl.Commons/GenerateBase64.cs
@@ -7,13 +7,7 @@ namespace BusesControl.Commons
     {
         public static string Generate<T>(T input)
         {
-            if (input is null)
-            {
-                return default!;
-            }
-
             var bytes = input is string ? Encoding.UTF8.GetBytes(input as string) : Encoding.UTF8.GetBytes(JsonSerializer.Serialize(input));
-
             return Convert.ToBase64String(bytes);
         }
     }
